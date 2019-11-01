84bb2f5307e01f9e6a06b6409aa203de9b738c03
Edvin Aginger <edvinaginger@hotmail.com>
2019-11-01 11:43:13 +0100

    gulp fully setup with browsersync and scss compiler

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,31 @@
-var gulp = require("gulp");
-//var uglify = require("gulp-uglify");
-var uglify = require("gulp-uglify-es").default;
-const concat =require("gulp-concat");
-
-let cleanCSS = require('gulp-clean-css');
-var autoprefix = require("gulp-autoprefixer");
-var plumber = require("gulp-plumber");
-var sourcemaps = require("gulp-sourcemaps");
-var sass = require('gulp-sass');
-//var uglify =require("gulp-uglify");
-sass.compiler = require('node-sass');
- 
-var JS_PATH = "public/**/*.js"
- 
-
-const CSS_PATH ="style/**/*.css"
-//styles
- 
- 
-gulp.task("styles", async function(){
-   console.log("styles is running");
-   //return gulp.src(["public/scss/reset/reset.scss", CSS_PATH])
-   return gulp.src(CSS_PATH)
-   .pipe(plumber(function (err){
-       console.log("Styles error", err);
-   }))
-   //.pipe(sourcemaps.init())
-   .pipe(autoprefix())
- //  .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-   //.pipe(sass().on('error', sass.logError))
-   .pipe(concat("styles.css"))
-   .pipe(cleanCSS({compatibility: 'ie8'}))
-  // .pipe(sourcemaps.write("./maps"))
-   .pipe(gulp.dest("public/dist"))
- 
-})
- 
-//scripts
-gulp.task("scripts", async function(){
-   return  gulp.src(JS_PATH)
-     
-       .pipe(uglify())
-       .on('error', function (err) { console.log( err ) })
-       .pipe(concat("app.js"))
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync").create();
 
 
-       .pipe(gulp.dest("public/dist"))
-      
-  
-    })
-     
-    //images
-    gulp.task("image", async function(){
-       console.log("image task is running");
-    });
-     
-    //default
-    gulp.task("default", async function(){
-       console.log("default task is running");
-    })
-     
-    //gulp watch
-     
-    gulp.task("watch", async function(){
-     
-       gulp.watch(JS_PATH, gulp.series("scripts"))
-       gulp.watch(CSS_PATH, gulp.series("styles"))
-    })
-    
+// Compile all SCSS vi har till CSS 
+function style() {
+  // hitta scss filerna 
+  return gulp.src('./sass/**/*.scss')
+    // skicka scss genom compilern
+  .pipe(sass())
+  // Vart ska jag spotta ut den nya CSS koden
+  .pipe(gulp.dest('./css'))
+  // stream ändringar till webbläsaren.
+  .pipe(browserSync.stream());
+}
+
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  gulp.watch('./sass/**/*.scss', style);
+  gulp.watch('./index.html').on('change', browserSync.reload);
+  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+
+// exportera så att du kan använda kommandon såsom 'gulp style'
+exports.style = style;
+exports.watch = watch;
---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+function countCost(product, vat) {
+    console.log(product+product*vat)
+    return product+product*vat
+}
+
+const vat = .25;
+const productOne = countCost(5000, vat);
+const productTwo = countCost(1000, vat);
+console.log("total cost: " + (productOne + productTwo))
+
+
+console.log("hej");
